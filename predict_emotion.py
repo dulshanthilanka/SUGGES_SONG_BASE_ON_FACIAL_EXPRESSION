f8edf73726581bef00f352257137c2ab9a6f78f2
THIWANKA DULSHAN <88085689+dulshanthilanka@users.noreply.github.com>
2024-02-14 17:22:09 +0530

    Update predict_emotion.py
    modification 1

--- predict_emotion.py
+++ predict_emotion.py
@@ -1,43 +1,20 @@
 import cv2
-import os
 import numpy as np
 import tensorflow as tf
+from PIL import Image as PilImage
 
-# Load the pre-trained model
-checkpoint_path = 'C:/Users/dulsh/Desktop/facial_expression/checkpoint/best_model.h5'
+checkpoint_path = 'C:/Users/dulsh/Documents/PROJECT_EUSL/pythonProject/model/best_model.h5'
 model = tf.keras.models.load_model(checkpoint_path)
 
-# Define emotion labels
 label_to_text = {0: 'anger', 1: 'disgust', 2: 'fear', 3: 'happiness', 4: 'sadness', 5: 'surprise', 6: 'neutral'}
 
-# Function to predict the expression for an image
-def predict_expression(image_path):
-    img = cv2.imread(image_path)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+def predict_expression(image):
+    img = np.array(image)
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     img = cv2.resize(img, (48, 48))
     img = img.reshape(1, 48, 48, 1)
     img = img / 255.0
 
-    # Predict the expression using the model
     prediction = model.predict(img)
     expression_label = label_to_text[np.argmax(prediction)]
-    return expression_label, img
-
-# Path to the photo you want to analyze (change this to your photo's path)
-photo_path = 'C:/Users/dulsh/Desktop/facial_expression/images/image1.jpg'
-
-# Predict the expression and get the image
-predicted_expression, image = predict_expression(photo_path)
-print("Predicted Expression:", predicted_expression)
-
-# Display the image with the predicted expression
-image_display = image.squeeze()
-image_display = (image_display * 255).astype(np.uint8)
-
-cv2.putText(image_display, predicted_expression, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
-# Use a different window name to avoid conflicts
-cv2.namedWindow('Predicted Expression', cv2.WINDOW_NORMAL)
-cv2.imshow('Predicted Expression', image_display)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    return expression_label
